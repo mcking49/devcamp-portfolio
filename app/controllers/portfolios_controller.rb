03d00a5c8dd2ce70b9f63ff7f1a0a83e39ab982a
Miten Chauhan <miten@chauhan.nz>
2020-10-26 12:02:41 +1300

    Integrated new and create actiosn for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,26 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_item_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ puts "9 portfolio items created"
     title: "Portfolio title #{portfolio_item}",
     subtitle: "My great service",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://via.placeholder.com/350x200",
-    thumb_image: "https://via.placeholder.com/600x400"
+    thumb_image: "https://via.placeholder.com/350x200",
+    main_image: "https://via.placeholder.com/600x400"
   )
 end
