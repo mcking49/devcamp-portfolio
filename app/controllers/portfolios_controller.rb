8e7523d42ba7cd8282b8cb892d1aa96272a73ced
Miten Chauhan <miten@chauhan.nz>
2020-10-26 16:15:29 +1300

    Implemented destroy action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,6 +38,14 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully destroyed.' }
+    end
+  end
+
   private
 
   def portfolio_item_params
